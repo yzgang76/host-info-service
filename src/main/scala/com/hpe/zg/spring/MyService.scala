4542eab60cdd2085c824f1b554733d97490cae6a
yzgang <yzgang76@sina.com>
2020-03-17 16:28:48 +0800

    v1

--- src/main/scala/com/hpe/zg/spring/MyService.scala
+++ src/main/scala/com/hpe/zg/spring/MyService.scala
@@ -3,6 +3,7 @@ package com.hpe.zg.spring
 import java.net.InetAddress
 
 import com.alibaba.fastjson.JSONObject
+import com.typesafe.config.ConfigFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
@@ -23,10 +24,23 @@ class indexController {
     def info(): JSONObject = {
         val localhost = InetAddress.getLocalHost
         import scala.jdk.CollectionConverters._
-        val json = new JSONObject(System.getenv.asScala.filter(entry=>entry._2 !=null).asJava.asInstanceOf[java.util.Map[String,Object]])
+        val json = new JSONObject(System.getenv.asScala.filter(entry => entry._2 != null).asJava.asInstanceOf[java.util.Map[String, Object]])
         json.put("hostName", localhost.getHostName)
         json.put("ip", localhost.getHostAddress)
         json.put("canonicalHostName", localhost.getCanonicalHostName)
+        json
+    }
+
+    @RequestMapping(value = Array("/conf"))
+    def conf(): JSONObject = {
+        val json = new JSONObject()
+        try {
+            val conf = ConfigFactory.parseFile(new java.io.File("C:\\mycode\\SpringService2\\src\\main\\docker\\cm/cm.conf")).resolve()
+            json.put("db.host", conf.getString("db.host"))
+            json.put("db.port", conf.getString("db.port"))
+        } catch {
+            case e: Throwable => json.put("error", e)
+        }
         json
     }
 }
